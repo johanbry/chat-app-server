76f37686f629110e77a1dbb0b9c1dbc46e319c7c
Johan Brynielsson <johan.brynielsson@gmail.com>
2023-08-16 11:10:20 +0200

    typescript dev setup

---
+++ server.js
@@ -0,0 +1,20 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const http_1 = require("http");
+const socket_io_1 = require("socket.io");
+const cors_1 = __importDefault(require("cors"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const app = (0, express_1.default)();
+const server = (0, http_1.createServer)(app);
+const io = new socket_io_1.Server(server, {
+    cors: {
+        origin: "*",
+    },
+});
+app.use((0, cors_1.default)());
+server.listen(process.env.PORT || 3000, () => console.log(`Server up and listening on port ${process.env.PORT}`));
--- server.ts
+++ server.ts
@@ -1,1 +1,21 @@
+import express, { Express } from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import cors from "cors";
+import dotenv from "dotenv";
 
+dotenv.config();
+
+const app: Express = express();
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+app.use(cors());
+
+server.listen(process.env.PORT || 3000, () =>
+  console.log(`Server up and listening on port ${process.env.PORT}`)
+);
